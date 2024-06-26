d8372b36a381ca825e526138982c0f236f77a123
Kaveesha-Boom <kaveesha9148@gmail.com>
2024-06-26 08:15:01 +0530

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -11,6 +11,6 @@ const username = fs.readFileSync('github_username.txt', 'utf8').trim()
 module.exports = {
     SESSION_ID:  process.env.SESSION_ID = "PRABATH-MD~8Xd1TJpC#F21dTxaJyZx7IqE3RzGAbsDYba2LM67-q_jTUwmOwSk",    
     BOT_NUMBER:  process.env.BOT_NUMBER = " 94727849423,
-    GITHUB_USERNAME: username = "Kaveesha-Boom",
+    
     
 };
