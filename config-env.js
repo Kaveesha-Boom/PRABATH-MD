7c70a45e10fcece9aed8162f530940eb88e45240
Kaveesha-Boom <kaveesha9148@gmail.com>
2024-06-26 07:42:37 +0530

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -9,8 +9,8 @@ function convertToBool(text, fault = 'true') {
 const username = fs.readFileSync('github_username.txt', 'utf8').trim()
 
 module.exports = {
-    SESSION_ID:  process.env.SESSION_ID,    
-    BOT_NUMBER:  process.env.BOT_NUMBER,
-    GITHUB_USERNAME: username,
-    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN,
+    SESSION_ID:  process.env.SESSION_ID = "PRABATH-MD~8Xd1TJpC#F21dTxaJyZx7IqE3RzGAbsDYba2LM67-q_jTUwmOwSk",    
+    BOT_NUMBER:  process.env.BOT_NUMBER = " 94727849423,
+    GITHUB_USERNAME: username = "Kaveesha-Boom",
+    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN = "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",
 };
