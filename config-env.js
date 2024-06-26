1e881505dd998b3c3e7ce819a5248886d4c51a23
Kaveesha-Boom <kaveesha9148@gmail.com>
2024-06-26 07:57:20 +0530

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -12,5 +12,5 @@ module.exports = {
     SESSION_ID:  process.env.SESSION_ID = "PRABATH-MD~8Xd1TJpC#F21dTxaJyZx7IqE3RzGAbsDYba2LM67-q_jTUwmOwSk",    
     BOT_NUMBER:  process.env.BOT_NUMBER = " 94727849423,
     GITHUB_USERNAME: username = "Kaveesha-Boom",
-    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN = "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",
+    
 };
